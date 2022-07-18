0808044b2c78ffd6238febec51c84206cd820df5
herosc <104774190+herosc@users.noreply.github.com>
2022-07-18 13:54:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function recordMessege (){
-    
+const puppeteer = require('puppeteer');
+
+async function bookFinder(){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // nome da variável temporário: Cosmos. Para fins de teste da aplicação no início. Pelo livro ter mais informações no site em que será realizada a busca (Amazon).
+    var bookTitle = "Cosmos"; 
+    await page.goto('https://amazon.com.br/');
+
+    await browser.close();
 }
+
+robo();
