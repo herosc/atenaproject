b88bdd74750b125551a95df6132c37692be1dbeb
Heros Carvalho <heros.carvalho@lendico.com.br>
2022-07-18 16:04:37 -0300

    Atualização de comentários

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const puppeteer = require('puppeteer');
 
+function countObjects (){
+    // Conta quantos objetos específicos aparecem na tela que está sendo exibida.
+}
+
 async function bookFinder(){
+
+    // Responsável por abrir o navegador, pesquisar pelo livro e preencher o formulário de forma automática.
+
     const browser = await puppeteer.launch({headless: false,});
     const page = await browser.newPage();
 
-    // nome da variável temporário: Cosmos. Para fins de teste da aplicação no início. Pelo livro ter mais informações no site em que será realizada a busca (Biblioteca Mário de Andrade).
     var bookSearch = "Cosmos";
+    // Variável bookSearch será definida em HardCode temporariamente para fins de teste. Porém deverá vir do formulário na tela da aplicação Catalogar Livro.
 
     var bookTitle = "";
     var authorName = "";
@@ -17,6 +24,7 @@ async function bookFinder(){
     await page.goto('http://bibliotecacircula.prefeitura.sp.gov.br/pesquisa/');
     await page.type ('[id="buscaSimples"]',bookSearch);
     await page.click ('[id="j_idt71"]',);
+    
     // await browser.close();
 }
 
