1366e6f199e773d75ea78ca88f86d6dfa703b431
Heros Carvalho <heros.carvalho@lendico.com.br>
2022-07-18 15:29:30 -0300

    Adiciona alterações referente a Biblioteca de Pesquisa (Mário de Andrade)

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const puppeteer = require('puppeteer');
 
 async function bookFinder(){
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: false,});
     const page = await browser.newPage();
 
-    // nome da variável temporário: Cosmos. Para fins de teste da aplicação no início. Pelo livro ter mais informações no site em que será realizada a busca (Amazon).
-    var bookTitle = "Cosmos"; 
-    await page.goto('https://amazon.com.br/');
+    // nome da variável temporário: Cosmos. Para fins de teste da aplicação no início. Pelo livro ter mais informações no site em que será realizada a busca (Biblioteca Mário de Andrade).
+    var bookSearch = "Cosmos";
 
-    await browser.close();
+    var bookTitle = "";
+    var authorName = "";
+    var publisherName = "";
+    var language = "";
+    var yearIndication = "";
+
+    // instruções gerais para procura do livro na base da Biblioteca Mário de Andrade.
+    await page.goto('http://bibliotecacircula.prefeitura.sp.gov.br/pesquisa/');
+    await page.type ('[id="buscaSimples"]',bookSearch);
+    await page.click ('[id="j_idt71"]',);
+    // await browser.close();
 }
 
-robo();
+bookFinder();
