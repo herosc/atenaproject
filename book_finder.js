d46cbbce57ded36314fbf80cfeb2146fe6812cac
Heros Carvalho <heros.carvalho@lendico.com.br>
2022-07-20 16:52:18 -0300

    Atualização faz com que informações catalogáveis sejam armazenadas em variáveis

---
+++ book_finder.js
@@ -0,0 +1,108 @@
+function showLog (report){
+    console.log(report);
+}
+
+
+const puppeteer = require('puppeteer');
+var bookSearch = "cosmos"; // Variável bookSearch será definida em HardCode temporariamente para fins de teste. Porém deverá vir do formulário na tela da aplicação Catalogar Livro.
+
+async function bookFinder(){
+
+    // Responsável por abrir o navegador, pesquisar pelo livro e preencher o formulário de forma automática.
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
+
+    // Instruções gerais para procura do livro no site da Amazon.
+    await page.goto ('https://www.amazon.com.br/'); 
+
+    page.setDefaultTimeout(3000);
+
+    await page.waitForSelector('input[type="text"]');
+    await page.click('input[type="text"]');
+    await page.keyboard.sendCharacter (bookSearch);
+    await page.click('input[type="submit"]');
+
+    // Mapeamento dos possíveis resultados provenientes de patrocínio na página da amazon feita nos três primeiros livros.
+
+    const firstElementSponsor = await page.waitForSelector('div[data-cel-widget="search_result_1"]');
+    const firstValue = await firstElementSponsor.evaluate(el => el.textContent);
+    const firstResult = firstValue.includes('Patrocinados');
+
+    const secondElementSponsor = await page.waitForSelector('div[data-cel-widget="search_result_2"]');
+    const secondValue = await secondElementSponsor.evaluate(el => el.textContent);
+    const secondResult = secondValue.includes('Patrocinados');
+
+    const thirdElementSponsor = await page.waitForSelector('div[data-cel-widget="search_result_3"]');
+    const thirdValue = await thirdElementSponsor.evaluate(el => el.textContent);
+    const thirdResult = thirdValue.includes('Patrocinados');
+    
+    const fourthElementSponsor = await page.waitForSelector('div[data-cel-widget="search_result_4"]');
+    const fourthValue = await fourthElementSponsor.evaluate(el => el.textContent);
+    const fourthResult = fourthValue.includes('Patrocinados');
+
+    let mapImageElements = await page.$$('[class="a-size-base-plus a-color-base a-text-normal"]');
+
+    // Define qual dos resultados da pesquisa é o mais próximo de ser o correto (excluindo os três primeiros anúncios).
+    if (firstResult == false){
+
+        await mapImageElements[0].click();
+
+    }else if (secondResult == false){
+
+        await mapImageElements[1].click();
+
+    }else if (thirdResult == false){
+
+        await mapImageElements[2].click();
+
+    }else if (fourthResult == false){
+
+        await mapImageElements[3].click();
+        
+    }else {
+
+        await mapImageElements[4].click();
+    }
+
+    try {
+        var avalibeTitle = await page.waitForSelector('span[id="productTitle"]');
+        var bookTitle = await avalibeTitle.evaluate(el => el.textContent);
+        console.log('O título do livro é '+bookTitle);
+    }catch{
+        console.log('Título não encontrado na página.');
+    }
+
+    try {
+        // var avaliblePublisher = await page.waitForSelector('');
+        var bookElements = await page.waitForXPath('//*[@id="detailBullets_feature_div"]/ul/li[1]/span/span[2]');
+        var publisherName = await bookElements.evaluate(el => el.textContent);
+        console.log('O nome da Editora é '+publisherName);
+    }catch{
+        console.log('Nome da editora não encontrado na busca.');
+    }
+
+    try {
+        var avalibleLanguage = await page.waitForXPath('//*[@id="detailBullets_feature_div"]/ul/li[2]/span/span[2]');
+        var language = await avalibleLanguage.evaluate(el => el.textContent);
+        console.log('O idioma do livro é '+language);
+    }catch{
+        console.log('Idioma do livro não disponível para consulta.');
+    }
+
+    try {
+        var avaliblePages = await page.waitForXPath('//*[@id="detailBullets_feature_div"]/ul/li[3]/span/span[2]');
+        var pages = await avaliblePages.evaluate(el => el.textContent);
+        console.log('O livro tem '+pages);
+    }catch{
+        console.log('Quantidade de páginas não encontrada na busca.');
+    }
+
+    
+
+    await browser.close();
+    console.log("FIM");
+    
+}
+
+bookFinder();
