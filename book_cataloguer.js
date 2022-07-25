d74cf5aeb51281cb76ba0fcc953a81b7d638bf55
Heros Carvalho <heros.carvalho@lendico.com.br>
2022-07-25 13:35:19 -0300

    Atualização do BookFinder para abrir o formulário temporário

--- book_cataloguer.js
+++ book_cataloguer.js
@@ -1,1 +1,17 @@
-const puppeteer = require('puppeteer');
+async function catalogue(completeTitle,amount,author,nPages,public,publisher,bookLang){
+
+    // const pageID = 'input[class="whsOnd zHQkBf"]';
+    const puppeteer = require('puppeteer');
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
+
+    await page.goto('https://forms.gle/Fh8EQhaFG3EmKETR9')
+    page.setDefaultTimeout(30000);
+    
+
+    await page.waitForSelector('div[class="aCsJod oJeWuf"]');
+    let field = await page.$$('div[class="aCsJod oJeWuf"]'); 
+    await field[1].click();
+    await page.keyboard.sendCharacter (completeTitle);
+}
--- book_finder.js
+++ book_finder.js
@@ -1,12 +1,29 @@
-function showLog (report){
-    console.log(report);
+// const puppeteer = require('puppeteer')
+
+// var catalogation = require('./book_cataloguer');
+
+async function catalogue(completeTitle){
+
+    // const pageID = 'input[class="whsOnd zHQkBf"]';
+    const puppeteer = require('puppeteer');
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
+
+    await page.goto('https://forms.gle/Fh8EQhaFG3EmKETR9')
+    page.setDefaultTimeout(30000);
+    
+
+    await page.waitForSelector('div[class="aCsJod oJeWuf"]');
+    let field = await page.$$('div[class="aCsJod oJeWuf"]'); 
+    await field[0].click();
+    await page.keyboard.sendCharacter (completeTitle);
 }
 
+async function bookFinder(searchTerm){
+    const puppeteer = require('puppeteer');
 
-const puppeteer = require('puppeteer');
-var bookSearch = "cafeteira";
-
-async function bookFinder(){
+    var bookSearch = searchTerm;
 
     // Responsável por abrir o navegador, pesquisar pelo livro e preencher o formulário de forma automática.
     const browser = await puppeteer.launch({headless: true});
@@ -100,9 +117,13 @@ async function bookFinder(){
 
     
 
-    await browser.close();
+    // await browser.close();
+
+    catalogue(bookTitle);
+
     console.log("FIM");
-    
 }
 
-bookFinder();
+
+
+bookFinder("Cosmos");
